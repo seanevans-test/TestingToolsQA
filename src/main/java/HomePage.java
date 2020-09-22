08060ba205b6c704faa684176adb105cf11d8e9c
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-22 18:44:57 +0100

    added Webdriver and findElement methods

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -5,4 +6,6 @@ public class HomePage {
     private WebDriver driver;
 
     public HomePage(WebDriver driver){this.driver = driver;}
+
+    private void clickLink(String linkText){driver.findElement(By.linkText(linkText)).click();}
 }
