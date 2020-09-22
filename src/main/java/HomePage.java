4366c73c70231fe546c6447b99d2ee87484cc4c4
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-22 19:02:00 +0100

    added links to each of the subPages via their WebElements

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -7,5 +7,30 @@ public class HomePage {
 
     public HomePage(WebDriver driver){this.driver = driver;}
 
+    public ElementsPage clickElementsPage(){
+        clickLink("Elements");
+        return new ElementsPage(driver);
+    }
+    public FormsPage clickFormsPage(){
+        clickLink("Forms");
+        return new FormsPage(driver);
+    }
+    public AlertsWindowsPage clickAlertsWindowsPage(){
+        clickLink("Alerts, Frame & Windows");
+        return new AlertsWindowsPage(driver);
+    }
+    public WidgetsPage clickWidgetsPage(){
+        clickLink("Widgets");
+        return new WidgetsPage(driver);
+    }
+    public InteractionPage clickInteractionPage(){
+        clickLink("Interactions");
+        return new InteractionPage(driver);
+    }
+    public BooksPage clickBooksPage(){
+        clickLink("Book Store Application");
+        return new BooksPage(driver);
+    }
+
     private void clickLink(String linkText){driver.findElement(By.linkText(linkText)).click();}
 }
